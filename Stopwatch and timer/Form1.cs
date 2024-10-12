2350e2af8744dca701ef275b83eb847e7165bf98
Kuno <80454713+Zevs2006@users.noreply.github.com>
2024-10-12 10:08:16 +0700

    Update Form1.cs

--- Stopwatch and timer/Form1.cs
+++ Stopwatch and timer/Form1.cs
@@ -5,97 +5,88 @@ namespace Stopwatch_and_timer
 {
     public partial class Form1 : Form
     {
-        private TimeSpan elapsedTime; // Для секундомера
-        private DateTime endTime; // Для таймера
-        private bool isRunning; // Статус секундомера
+        private TimeSpan timeSpan;
+        private bool isRunning;
 
         public Form1()
         {
             InitializeComponent();
-            elapsedTime = TimeSpan.Zero;
+            timeSpan = TimeSpan.Zero;
             isRunning = false;
-            labelTime.Text = elapsedTime.ToString(@"hh\:mm\:ss");
-            labelTimer.Text = "00:00:00"; // Изначально таймер не запущен
         }
 
         private void buttonStartStop_Click(object sender, EventArgs e)
         {
             if (isRunning)
             {
-                // Остановить секундомер
                 timer1.Stop();
-                buttonStartStop.Text = "Старт";
+                buttonStartStop.Text = "РЎС‚Р°СЂС‚";
             }
             else
             {
-                // Запустить секундомер
                 timer1.Start();
-                buttonStartStop.Text = "Остановить";
+                buttonStartStop.Text = "РЎС‚РѕРї";
             }
             isRunning = !isRunning;
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            elapsedTime = elapsedTime.Add(TimeSpan.FromSeconds(1));
-            labelTime.Text = elapsedTime.ToString(@"hh\:mm\:ss");
+            timeSpan = timeSpan.Add(TimeSpan.FromSeconds(1));
+            labelTime.Text = timeSpan.ToString(@"d\:hh\:mm\:ss"); // Р’С‹РІРѕРґ СЃ СѓС‡РµС‚РѕРј РґРЅРµР№
         }
 
         private void buttonReset_Click(object sender, EventArgs e)
         {
-            // Сбросить секундомер
             timer1.Stop();
-            elapsedTime = TimeSpan.Zero;
-            labelTime.Text = elapsedTime.ToString(@"hh\:mm\:ss");
-            buttonStartStop.Text = "Старт";
+            timeSpan = TimeSpan.Zero;
+            labelTime.Text = "00:00:00";
             isRunning = false;
+            buttonStartStop.Text = "РЎС‚Р°СЂС‚";
         }
 
         private void buttonSetTimer_Click(object sender, EventArgs e)
         {
-            // Установить таймер на выбранное количество секунд
-            int seconds = (int)numericUpDownTime.Value;
-            endTime = DateTime.Now.AddSeconds(seconds);
-            labelTimer.Text = TimeSpan.FromSeconds(seconds).ToString(@"hh\:mm\:ss");
+            // РџРѕР»СѓС‡Р°РµРј РѕР±С‰РµРµ РІСЂРµРјСЏ РІ РјРёРЅСѓС‚Р°С…, Рё РїСЂРµРѕР±СЂР°Р·СѓРµРј РІ TimeSpan
+            timeSpan = TimeSpan.FromSeconds((double)numericUpDownTime.Value);
+            labelTimer.Text = timeSpan.ToString(@"d\:hh\:mm\:ss"); // Р’С‹РІРѕРґ СЃ СѓС‡РµС‚РѕРј РґРЅРµР№
         }
 
         private void buttonStartTimer_Click(object sender, EventArgs e)
         {
-            // Запустить таймер
             timer2.Start();
-            timer2.Tick += Timer2_Tick; // Привязка обработчика событий для таймера
         }
 
         private void buttonStopTimer_Click(object sender, EventArgs e)
         {
-            // Остановить таймер
             timer2.Stop();
         }
 
         private void buttonResetTimer_Click(object sender, EventArgs e)
         {
-            // Сбросить таймер
             timer2.Stop();
+            timeSpan = TimeSpan.Zero;
             labelTimer.Text = "00:00:00";
-            numericUpDownTime.Value = 0;
         }
 
         private void Timer2_Tick(object sender, EventArgs e)
         {
-            // Проверка, не истек ли таймер
-            if (DateTime.Now >= endTime)
+            if (timeSpan.TotalSeconds > 0)
             {
-                timer2.Stop();
-                MessageBox.Show("Таймер истёк!", "Уведомление", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                labelTimer.Text = "00:00:00"; // Сброс таймера
+                timeSpan = timeSpan.Add(TimeSpan.FromSeconds(-1));
+                labelTimer.Text = timeSpan.ToString(@"d\:hh\:mm\:ss"); // Р’С‹РІРѕРґ СЃ СѓС‡РµС‚РѕРј РґРЅРµР№
             }
             else
             {
-                TimeSpan remainingTime = endTime - DateTime.Now;
-                labelTimer.Text = remainingTime.ToString(@"hh\:mm\:ss");
+                timer2.Stop();
+                MessageBox.Show("Р’СЂРµРјСЏ РІС‹С€Р»Рѕ!");
             }
         }
 
-        
+        private void buttonLap_Click(object sender, EventArgs e)
+        {
+            // Р”РѕР±Р°РІР»СЏРµРј С‚РµРєСѓС‰РµРµ РІСЂРµРјСЏ С„Р»Р°Р¶РєР° РІ СЃРїРёСЃРѕРє
+            listBoxLaps.Items.Add(labelTime.Text);
+        }
     }
 }
